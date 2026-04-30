2bd24a50b09867b2633f057afa5fba66d057d790
Simon Forsberg <simon.co.forsberg@gmail.com>
2026-04-30 14:10:40 +0200

    initial commit: initialize Spring Boot project

---
+++ src/main/java/org/example/promptly/PromptlyApplication.java
@@ -0,0 +1,13 @@
+package org.example.promptly;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromptlyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PromptlyApplication.class, args);
+    }
+
+}
